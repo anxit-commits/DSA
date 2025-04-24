02fed4766c7a307cc6fa0319faad538d2d9258e9
ankit-commits <ankitkm2412@gamil.com>
2025-04-24 15:57:44 +0530

    second commit

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+//     int a;
+//     int b;
+   
+//     cout<<"Enter the value of a"<<endl;
+//     cin>>a;
+//     cout<<"Enter the value of b"<<endl;
+//     cin>>b;
+//     int sum = a+b;
+// cout<<"sum="<<sum<<endl;
+// return 0;
+// }
+// int a = 10;
+
+// int b = a--;
+// cout<<"b = " <<b<<endl;
+// cout<<"a="<<a<<endl;
+// return 0;
